05350f9b8e72e46dce708fe61db714095901491a
Ankit <ankitc1792@gmail.com>
2023-03-17 17:45:09 +0530

    created theme class and added models

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:tutorial_1/pages/home_page.dart';
 import 'package:tutorial_1/pages/login_page.dart';
 import "package:google_fonts/google_fonts.dart";
 import 'package:tutorial_1/utils/routes.dart';
+import 'package:tutorial_1/widgets/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,11 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: Mytheme.lightTheme(context),
+      darkTheme: Mytheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: Myroutes.homeRoute,
       routes: {
